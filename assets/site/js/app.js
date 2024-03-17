33185f95fc7b39853f28568af2b47b4f7fb12983
DanHariton <danilhariton@gmail.com>
2024-03-17 21:09:11 +0100

    * responsive design

--- assets/site/js/app.js
+++ assets/site/js/app.js
@@ -1,5 +1,5 @@
 window.$ = window.jQuery = require('jquery/dist/jquery');
-require('bootstrap/dist/js/bootstrap.bundle');
-
+import 'bootstrap/dist/js/bootstrap.bundle';
+require('./components/hamburger.js');
 import AOS from 'aos';
 AOS.init();
