e609cff145868adefea04182fbcfb33f544dc948
DanHariton <danilhariton@gmail.com>
2024-03-20 23:28:49 +0100

    * fixes fe by feedback

--- assets/site/js/components/showConcerts.js
+++ assets/site/js/components/showConcerts.js
@@ -1,10 +1,16 @@
-var showConcertsButton = document.getElementById("show-concerts");
-var pastConcertsDiv = document.getElementById("past-concerts");
+import AOS from 'aos';
+AOS.init();
+let showConcertsButton = document.getElementById("show-concerts");
+let pastConcertsDiv = document.getElementById("past-concerts");
 
 showConcertsButton.addEventListener("click", function() {
     if (pastConcertsDiv.classList.contains("d-none")) {
         pastConcertsDiv.classList.remove("d-none");
         showConcertsButton.textContent = "Skrýt odehrané koncerty";
+
+        let pastConcertItems = document.querySelectorAll('.past-concert-item');
+        let pastConcertItemsArray = Array.from(pastConcertItems);
+        AOS.refreshHard(pastConcertItemsArray);
     } else {
         pastConcertsDiv.classList.add("d-none");
         showConcertsButton.textContent = "Odehrané koncerty";
