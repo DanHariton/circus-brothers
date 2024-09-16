0c1ab55017f59459850e6ea6454a8792e804c436
DanHariton <danilhariton@gmail.com>
2024-09-16 20:59:34 +0200

    * video preview * meta * colors

--- assets/site/js/components/videoPlay.js
+++ assets/site/js/components/videoPlay.js
@@ -7,3 +7,22 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById('banner-vid');
+    var canvas = document.getElementById('video-canvas');
+    var ctx = canvas.getContext('2d');
+    var videoContainer = document.querySelector('.video-container');
+
+    video.addEventListener('loadeddata', function() {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    });
+
+    video.oncanplay = function() {
+        canvas.style.display = 'none';
+        videoContainer.style.display = 'block';
+    };
+});
